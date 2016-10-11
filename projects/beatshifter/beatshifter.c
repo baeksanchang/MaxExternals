b0a4be692ef8438c6c7f1be30f8be8213873152d
Baek San Chang <baek_chang@apple.com>
2016-10-11 16:41:37 -0700

    Use list inlet to parse parameter values for each time interval Also add chance value to add randomization of applying offsets

--- projects/beatshifter/beatshifter.c
+++ projects/beatshifter/beatshifter.c
@@ -9,18 +9,25 @@
 #include "ext.h"							// standard Max include, always required
 #include "ext_obex.h"						// required for new style Max object
 
+typedef struct _interval_data
+{
+	double onbeat_offset;
+	double offbeat_offset;
+	double onbeat_chance;
+	double offbeat_chance;
+} t_interval_data;
+
 ////////////////////////// object struct
 typedef struct _beatshifter
 {
 	t_object	beatshifter; // the object itself (must be first)
 	double		tempo;
 	int			ticks;
-	double		quarter_offset;
-	double		quarter_offbeat_offset;
-	double		eighth_offset;
-	double		eighth_offbeat_offset;
-	double		sixteenth_offset;
-	double		sixteenth_offbeat_offset;
+
+	t_interval_data quarter;
+	t_interval_data eighth;
+	t_interval_data sixteenth;
+
 	double		delay_amount;
 	void *		delay_outlet;
 } t_beatshifter;
@@ -29,14 +36,7 @@ typedef struct _beatshifter
 void *bs_new();
 void bs_tempo(t_beatshifter *b, double tempo);
 void bs_ticks(t_beatshifter *b, double ticks);
-void bs_quarter(t_beatshifter *b, double value);
-void bs_quarter_offbeat(t_beatshifter *b, double value);
-void bs_8th(t_beatshifter *b, double value);
-void bs_8th_offbeat(t_beatshifter *b, double value);
-void bs_16th(t_beatshifter *b, double value);
-void bs_16th_offbeat(t_beatshifter *b, double value);
-
-
+void parse_values(t_beatshifter *b, t_symbol *sym, long argc, t_atom *argv);
 void bs_compute_delay(t_beatshifter *b);
 
 //////////////////////// global class pointer variable
@@ -54,12 +54,8 @@ int C74_EXPORT main(void)
 
 	class_addmethod(c, (method)bs_tempo, "ft1", A_FLOAT, 0);
 	class_addmethod(c, (method)bs_ticks, "ft2", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_quarter, "ft3", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_quarter_offbeat, "ft4", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_8th, "ft5", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_8th_offbeat, "ft6", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_16th, "ft7", A_FLOAT, 0);
-	class_addmethod(c, (method)bs_16th_offbeat, "ft8", A_FLOAT, 0);
+
+	class_addmethod(c, (method)parse_values, "anything", A_GIMME, 0);
 
 	gThis = c;
 
@@ -70,12 +66,7 @@ void *bs_new()
 {
 	t_beatshifter *b = (t_beatshifter *)object_alloc(gThis);
 
-	floatin(b, 8);
-	floatin(b, 7);
-	floatin(b, 6);
-	floatin(b, 5);
-	floatin(b, 4);
-	floatin(b, 3);
+	inlet_new(b, NULL);
 	floatin(b, 2);
 	floatin(b, 1);
 
@@ -83,12 +74,22 @@ void *bs_new()
 
 	b->tempo = 0;
 	b->ticks = 0;
-	b->quarter_offset = 0;
-	b->quarter_offbeat_offset = 0;
-	b->eighth_offset = 0;
-	b->eighth_offbeat_offset = 0;
-	b->sixteenth_offset = 0;
-	b->sixteenth_offbeat_offset = 0;
+
+	b->quarter.onbeat_offset = 0.0;
+	b->quarter.offbeat_offset = 0.0;
+	b->quarter.onbeat_chance = 100.0;
+	b->quarter.offbeat_chance = 100.0;
+
+	b->eighth.onbeat_offset = 0.0;
+	b->eighth.offbeat_offset = 0.0;
+	b->eighth.onbeat_chance = 100.0;
+	b->eighth.offbeat_chance = 100.0;
+
+	b->sixteenth.onbeat_offset = 0.0;
+	b->sixteenth.offbeat_offset = 0.0;
+	b->sixteenth.onbeat_chance = 100.0;
+	b->sixteenth.offbeat_chance = 100.0;
+
 	b->delay_amount = 0;
 
 	return b;
@@ -96,11 +97,9 @@ void *bs_new()
 
 void bs_tempo(t_beatshifter *b, double tempo)
 {
-//	post("Tempo %f", tempo);
 	if (b->tempo != tempo)
 	{
 		b->tempo = tempo;
-		bs_compute_delay(b);
 	}
 }
 
@@ -114,46 +113,68 @@ void bs_ticks(t_beatshifter *b, double ticks)
 	}
 }
 
-void bs_quarter(t_beatshifter *b, double value)
+void parse_values(t_beatshifter *b, t_symbol *sym, long argc, t_atom *argv)
 {
-//	post("quarter offset %f", value);
-	b->quarter_offset = value;
-	bs_compute_delay(b);
-}
+	long i;
+	t_atom *ap;
 
-void bs_quarter_offbeat(t_beatshifter *b, double value)
-{
-//	post("quarter offbeat offset %f", value);
-	b->quarter_offbeat_offset = value;
-	bs_compute_delay(b);
-}
+	t_interval_data *interval_data = NULL;
 
-void bs_8th(t_beatshifter *b, double value)
-{
-//	post("8th offset %f", value);
-	b->eighth_offset = value;
-	bs_compute_delay(b);
-}
+	const char *sym_name = sym->s_name;
+	if (strcmp("quarter", sym_name) == 0)
+	{
+		interval_data = &b->quarter;
+	}
+	else if (strcmp("eighth", sym_name) == 0)
+	{
+		interval_data = &b->eighth;
+	}
+	else if (strcmp("sixteenth", sym_name) == 0)
+	{
+		interval_data = &b->sixteenth;
+	}
 
-void bs_8th_offbeat(t_beatshifter *b, double value)
-{
-//	post("8th offbeat offset %f", value);
-	b->eighth_offbeat_offset = value;
-	bs_compute_delay(b);
-}
+	if (interval_data == NULL)
+	{
+		post("ERROR, interval data is NULL!");
+		return;
+	}
 
-void bs_16th(t_beatshifter *b, double value)
-{
-//	post("16th offset %f", value);
-	b->sixteenth_offset = value;
-	bs_compute_delay(b);
-}
+	for (i = 0, ap = argv; i < argc; i++, ap++)
+	{
+		switch (atom_gettype(ap))
+		{
+			case A_FLOAT:
+			{
+				switch (i)
+				{
+					case 0:
+						interval_data->onbeat_offset = argv[i].a_w.w_float;
+						break;
 
-void bs_16th_offbeat(t_beatshifter *b, double value)
-{
-//	post("16th offbeat offset %f", value);
-	b->sixteenth_offbeat_offset = value;
-	bs_compute_delay(b);
+					case 1:
+						interval_data->offbeat_offset = argv[i].a_w.w_float;
+						break;
+
+					case 2:
+						interval_data->onbeat_chance = argv[i].a_w.w_float;
+						break;
+
+					case 3:
+						interval_data->offbeat_chance = argv[i].a_w.w_float;
+						break;
+
+					default:
+						break;
+				}
+				break;
+			}
+
+			default:
+				post("%ld: unknown atom type (%ld)", i+1, atom_gettype(ap));
+				break;
+		}
+	}
 }
 
 void bs_compute_delay(t_beatshifter *b)
@@ -161,42 +182,61 @@ void bs_compute_delay(t_beatshifter *b)
 	// default delay amount by largest offset which is 8th note
 	double delay = 60000.0 / 2.0 / b->tempo;
 
-//	post("computing delay: ticks %d", b->ticks);
+	srand(time(NULL));
+	double rand_chance_value = (double)(rand() % 100);
 
 	switch (b->ticks)
 	{
 		case 15:
 		case 31:
-			delay += (60000.0/b->tempo)*b->quarter_offset;
+			if (rand_chance_value <= b->quarter.onbeat_chance)
+			{
+				delay += (60000.0/b->tempo)*b->quarter.onbeat_offset;
+			}
 			break;
 
 		case 7:
 		case 23:
-			delay += (60000.0/b->tempo)*b->quarter_offbeat_offset;
+			if (rand_chance_value <= b->quarter.offbeat_chance)
+			{
+				delay += (60000.0/b->tempo)*b->quarter.offbeat_offset;
+			}
 			break;
 
 		case 3:
 		case 19:
-			delay += (60000.0/2.0/b->tempo)*b->eighth_offset;
+			if (rand_chance_value <= b->eighth.onbeat_chance)
+			{
+				delay += (60000.0/2.0/b->tempo)*b->eighth.onbeat_offset;
+			}
 			break;
 
 		case 11:
 		case 27:
-			delay += (60000.0/2.0/b->tempo)*b->eighth_offbeat_offset;
+			if (rand_chance_value <= b->eighth.offbeat_chance)
+			{
+				delay += (60000.0/2.0/b->tempo)*b->eighth.offbeat_offset;
+			}
 			break;
 
 		case 1:
 		case 9:
 		case 17:
 		case 25:
-			delay += (60000.0/4.0/b->tempo)*b->sixteenth_offset;
+			if (rand_chance_value <= b->sixteenth.onbeat_chance)
+			{
+				delay += (60000.0/4.0/b->tempo)*b->sixteenth.onbeat_offset;
+			}
 			break;
 
 		case 5:
 		case 13:
 		case 21:
 		case 29:
-			delay += (60000.0/4.0/b->tempo)*b->sixteenth_offbeat_offset;
+			if (rand_chance_value <= b->sixteenth.offbeat_chance)
+			{
+				delay += (60000.0/4.0/b->tempo)*b->sixteenth.offbeat_offset;
+			}
 			break;
 
 		// use delay value already set
